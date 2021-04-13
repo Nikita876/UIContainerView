08c9fde154f53e98a38317a5c033de6ce91087f3
nikita_kogolenok <nikiton17ns@gmail.com>
2021-04-13 10:43:37 +0300

    Initial Commit

---
+++ UIContainerView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UIContainerView
+//
+//  Created by Никита Коголенок on 1.02.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    // MARK: - Outlet
+    @IBOutlet weak var imageCover: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+    // MARK: - Lifecycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
